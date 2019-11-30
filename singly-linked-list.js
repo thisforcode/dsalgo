b7156b73499235d75110e59f5a066d0db2dc722a
thisforcode <thisforcode@gmail.com>
2019-11-30 12:04:04 +0530

    SSL-add-beginnig-end

--- singly-linked-list.js
+++ singly-linked-list.js
@@ -4,3 +4,32 @@ class Node{
 		this.next = null;
 	}
 }
+
+class LinkedList{
+	constructor(){
+		this.head = null;
+	}
+	addAtHead(data){
+		let newNode = new Node(data);
+		newNode.next = this.head;
+		this.head = newNode;
+		return this.head;
+	}
+	addAtTail(data){
+		let newNode = new Node(data);
+		if(!this.head){
+			this.head = newNode;
+			return this.head;
+		}
+		let tail = this.head;
+		if(tail.next !== null){
+			tail = tail.next;
+		}
+		tail.next = newNode;	
+
+		return this.head;
+	}
+}
+var list = new LinkedList();
+list.addAtHead(5);
+console.log(list.addAtTail(4));
